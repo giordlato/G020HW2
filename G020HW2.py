a8cb7ea60e8fce192277ae7536c9ae7f3b64777a
giordlato <165940321+giordlato@users.noreply.github.com>
2024-05-07 22:16:46 +0200

    Add files via upload

--- G020HW2.py
+++ G020HW2.py
@@ -1,12 +1,15 @@
+
 import math
 import random
 from timeit import default_timer as timer
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkContext, SparkConf, StorageLevel
 import sys
 import os
 
 
 def MRFFT(inputPoints, K, sc):
+
+    #Compute local centers using SequentialFFT, we persist and count to keep R1 coherent.
 
     def local_fft(partition):
         points = list(partition)
@@ -18,7 +21,7 @@ def MRFFT(inputPoints, K, sc):
     start1 = timer()
     local_centers_rdd = inputPoints.mapPartitions(local_fft)
     reduced_centers = local_centers_rdd.map(lambda center: (1, [center])).reduceByKey(lambda a, b: a + b)
-    reduced_centers.cache()
+    reduced_centers.persist(StorageLevel.MEMORY_AND_DISK)
     reduced_centers.count()
     end1 = timer()
     print("Running time of MRFFT Round 1 =", math.floor((end1 - start1) * 1000), "ms")
@@ -26,72 +29,47 @@ def MRFFT(inputPoints, K, sc):
     # Round 2: Compute final centers from the reduced set of local centers
     start2 = timer()
     reduced_centers = reduced_centers.flatMap(lambda x: x[1]).collect()
-    centers = SequentialFFT(reduced_centers,K)
+    C = SequentialFFT(reduced_centers,K)
     end2 = timer()
-    centers_broadcast = sc.broadcast(centers)
+    centers_broadcast = sc.broadcast(C)
     print("Running time of MRFFT Round 2 =", math.floor((end2 - start2) * 1000), "ms")
 
     # Round 3: Compute the radius of the clustering
-    start3 = timer()
+
 
     def min_distance(point):
         return min(math.dist(point, center) for center in centers_broadcast.value)
 
-    radius = inputPoints.map(min_distance).reduce(max)
+    start3 = timer()
+    R = inputPoints.map(min_distance).reduce(max)
     end3 = timer()
-    print("Radius =", radius)
     print("Running time of MRFFT Round 3 =", math.floor((end3 - start3) * 1000), "ms")
-    return radius
+    print("Radius =", R)
+    return R
 
-'''def SequentialFFT(P,K):
-    C = []
-    C.append(random.choice(P))
-    for i in range(0,K-1):
-        max_dist = 0
-        max_elem = 0
-        for j in range(len(P)):
-            distance = math.dist(C[i], P[j])
-            for m in range(0, len(C)-1):
-                new_distance = math.dist(C[m], P[j])
-                if new_distance < distance:
-                    distance = new_distance
-            if distance > max_dist:
-                max_dist = distance
-                max_elem = P[j]
-        C.append(max_elem)
-    return C'''
-
+'''SequentialFFT uses a list to save P points and P closest distances, this ensures the P*k speed, 
+   setting the default distances to infinity'''
 def SequentialFFT(P,K):
     C = []
-    Tabella = []
+    Table = []
     for i in range (len(P)):
-        Tabella.append([P[i],0,0])
+        Table.append([P[i],float('inf')])
     C.append(random.choice(P))
-    for i in range(0,K-1):
-        max_dist = 0
-        max_elem = 0
+    for i in range(K-1):
         for j in range(len(P)):
-            a = Tabella [j][2]
-            start1 = timer()
             distance = math.dist(C[i], P[j])
-            end1 =timer()
-            print(end1-start1)
-            if i == 0:
-                Tabella[j][2] = distance
-            if distance < a:
-                Tabella[j][2] = distance
-
-            print(end1 - start1)
-            if Tabella[j][2] > max_dist:
-                max_dist = a
-                max_elem = P[j]
+            if distance < Table[j][1]:
+                Table[j][1] = distance
+        max_sublist = (max(Table, key=lambda x: x[1]))
+        max_elem = max_sublist[0]
         C.append(max_elem)
     return C
+
+'''Methods for MRApproxOutliers'''
 def get_cell_id(x, y, Lambda):
     i = math.floor(x / Lambda)
     j = math.floor(y / Lambda)
     return (i, j)
-
 
 # STEP A
 def count_points_within_partition(iterator, Lambda):
@@ -110,7 +88,6 @@ def count_points_within_partition(iterator, Lambda):
     for cell_id, count in cell_counts.items():
         yield (cell_id, count)
 
-
 # count the total points in each cell in all L partitions
 def count_points_in_cells(points, Lambda):
     # uses RDD's mapPartitions method to apply count_points_within_partition to each partition
@@ -120,7 +97,6 @@ def count_points_in_cells(points, Lambda):
     total_cell_counts = local_cell_counts.reduceByKey(lambda a, b: a + b)
     # returns the aggregate counts. Contains pairs of cell IDs and their total point count.
     return total_cell_counts
-
 
 # STEP B
 def extend_with_neighbour_counts(cell_counts, M):
@@ -144,8 +120,6 @@ def extend_with_neighbour_counts(cell_counts, M):
 
 
 '''MR ApproxOutliers manages to count points in L partitions and add N3 and N7, doing so in two steps.'''
-
-
 def MRApproxOutliers(points_rdd, D, M):
     Lambda = D / (2 * math.sqrt(2))
     cell_counts = count_points_in_cells(points_rdd, Lambda)
@@ -163,14 +137,13 @@ def main():
     except:
         print("Incorrect type of parameters")
         exit(1)
-    conf = SparkConf().setAppName('G020HW1')
+    conf = SparkConf().setAppName('G020HW2')
     sc = SparkContext(conf=conf)
     conf.set("spark.locality.wait", "0s");
-    rawData = sc.textFile(data_path).repartition(L).cache()
+    rawData = sc.textFile(data_path).repartition(L).persist(StorageLevel.MEMORY_AND_DISK)
     inputPoints = rawData.map(lambda x: [float(i) for i in x.split(",")])
     print("Number of points =", inputPoints.count())
     D = MRFFT(inputPoints, K, sc)
-    print("Computed radius (D) for MRApproxOutliers:", D)
     start1 = timer()
     MRApproxOutliers(inputPoints, D, M)
     end1 = timer()
